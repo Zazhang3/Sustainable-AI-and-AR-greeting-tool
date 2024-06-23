685d0890dab9501de1f85fb25dd9ef7f146a760c
whywxy7 <104261300+whywxy7@users.noreply.github.com>
2024-06-23 17:24:40 +0100

    ADD HomeController.javan

---
+++ greeting-tool/server/src/main/java/com/tool/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.tool.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello, World!";
+    }
+}
