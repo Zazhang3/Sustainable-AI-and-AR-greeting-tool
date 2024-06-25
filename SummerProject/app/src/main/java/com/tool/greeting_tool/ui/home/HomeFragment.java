d83189aed7c4c95993eb3beaa77297956d7a590f
Zhang Ziang <zhangziang139@gmail.com>
2024-06-25 16:47:30 +0100

    Finnish send card(need data from front end)

--- SummerProject/app/src/main/java/com/tool/greeting_tool/ui/home/HomeFragment.java
+++ SummerProject/app/src/main/java/com/tool/greeting_tool/ui/home/HomeFragment.java
@@ -3,6 +3,7 @@ package com.tool.greeting_tool.ui.home;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,18 +18,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.gson.Gson;
 import com.tool.greeting_tool.MainActivity;
 import com.tool.greeting_tool.Postcode_fill;
 import com.tool.greeting_tool.WordsSelect;
 import com.tool.greeting_tool.common.constant.ErrorMessage;
 import com.tool.greeting_tool.common.constant.KeySet;
+import com.tool.greeting_tool.common.constant.TAGConstant;
+import com.tool.greeting_tool.common.constant.URLConstant;
+import com.tool.greeting_tool.common.utils.SharedPreferencesUtil;
 import com.tool.greeting_tool.databinding.FragmentHomeBinding;
+import com.tool.greeting_tool.pojo.dto.GreetingCard;
 import com.tool.greeting_tool.server.GeocodingServer;
 import com.tool.greeting_tool.server.LocationHelper;
 
+import java.io.IOException;
 import java.security.Key;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class HomeFragment extends Fragment {
 
@@ -124,6 +139,9 @@ public class HomeFragment extends Fragment {
             //These are the items need for back_end
             String postCode = data.getStringExtra(KeySet.PostKey);
             ArrayList<String> SelectedItems = data.getStringArrayListExtra(KeySet.SelectedList);
+           // assert (postCode != null);
+            //assert (SelectedItems != null);
+            sendGreetingCard(SelectedItems,postCode);
         }
     }
 
@@ -131,6 +149,64 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    private void sendGreetingCard(ArrayList<String> selectionList, String postcode) {
+
+        //Just for test, disable it while it can receive message
+        GreetingCard greetingCard = new GreetingCard();
+        greetingCard.setText("Happy Birthday!");
+        greetingCard.setEmoji("Laugh");
+        greetingCard.setAnimation("rotate");
+        greetingCard.setPostcode("BS2 0BU");
+        greetingCard.setUser_id(SharedPreferencesUtil.getLong(requireContext()));
+
+        /*GreetingCard greetingCard = new GreetingCard();
+        greetingCard.setText(selectionList.get(0));
+        greetingCard.setEmoji(selectionList.get(1));
+        greetingCard.setAnimation(selectionList.get(2));
+        greetingCard.setPostcode(postcode);
+        greetingCard.setUser_id(SharedPreferencesUtil.getLong(requireContext()));*/
+
+        Gson gson = new Gson();
+        String json = gson.toJson(greetingCard);
+        String jwtToken = SharedPreferencesUtil.getToken(requireContext());
+
+        RequestBody body = RequestBody.create(json,
+                MediaType.get("application/json; charset=utf-8"));
+
+        Request request = new Request.Builder()
+                .url(URLConstant.SEND_CARD_URL)
+                .header("Token",jwtToken)
+                .post(body)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.e(TAGConstant.SEND_CARD_TAG,"send card failed",e);
+                requireActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(requireContext(), "Failed to send card", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    getActivity().runOnUiThread(() -> Toast.makeText(getActivity(), "Greeting card sent successfully", Toast.LENGTH_SHORT).show());
+                } else {
+                    getActivity().runOnUiThread(() -> Toast.makeText(getActivity(), "Failed to send greeting card", Toast.LENGTH_SHORT).show());
+                }
+            }
+
+        });
+
+
     }
 
 }
