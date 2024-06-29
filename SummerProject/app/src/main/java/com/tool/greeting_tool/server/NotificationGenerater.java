a27a801ad53a94a067fe73912b961023efe1fa68
Quillan-Liu <892664998lqy@gmail.com>
2024-06-29 20:12:42 +0100

    Add content in Front_Dev to Notification_Front_dev

--- SummerProject/app/src/main/java/com/tool/greeting_tool/server/NotificationGenerater.java
+++ SummerProject/app/src/main/java/com/tool/greeting_tool/server/NotificationGenerater.java
@@ -14,6 +14,7 @@ import com.tool.greeting_tool.R;
 import com.tool.greeting_tool.ui.home.HomeFragment;
 import com.tts.TextToSpeechConverter;
 
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Timer;
