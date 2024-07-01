719323059f3da1c1ebecc4cd4ee21af4aa8ca225
HankeXian <154899499+HankeXian@users.noreply.github.com>
2024-07-02 00:45:05 +0100

    add TTS function
    Now it can make sound with postcode,
    it will make a sound immediately after launch the app because the use of SharedPreferencesUtil

--- SummerProject/app/src/main/java/com/tool/greeting_tool/ui/home/HomeFragment.java
+++ SummerProject/app/src/main/java/com/tool/greeting_tool/ui/home/HomeFragment.java
@@ -1,7 +1,11 @@
 package com.tool.greeting_tool.ui.home;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,12 +16,18 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.watson.text_to_speech.v1.TextToSpeech;
+import com.ibm.watson.text_to_speech.v1.model.SynthesizeOptions;
+import com.ibm.watson.text_to_speech.v1.util.WaveUtils;
 import com.tool.greeting_tool.WordsSelect;
 import com.tool.greeting_tool.common.constant.ErrorMessage;
 import com.tool.greeting_tool.common.constant.KeySet;
@@ -27,10 +37,15 @@ import com.tool.greeting_tool.common.utils.SharedPreferencesUtil;
 import com.tool.greeting_tool.databinding.FragmentHomeBinding;
 import com.tool.greeting_tool.pojo.dto.GreetingCard;
 import com.tool.greeting_tool.server.LocationHelper;
+import com.tool.greeting_tool.server.TextToSpeechHelper;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 import okhttp3.Call;
@@ -45,8 +60,16 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     private LocationHelper locationHelper;
+
+    private TextToSpeechHelper textToSpeechHelper;
+
+    private MediaPlayer mediaPlayer;
+    private String audioPath;
     private static final int REQUEST_CODE_SELECT_1 = 1;
     private static final int REQUEST_CODE_SELECT_2 = 2;
+
+    private static final int REQUEST_WRITE_STORAGE = 112;
+
     private ArrayList<GreetingCard> nearbyGreetingCards = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -58,6 +81,24 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
 
         locationHelper = new LocationHelper(requireContext());
+
+        String postcode_notification = SharedPreferencesUtil.getNotificationMessage(requireContext());
+        textToSpeechHelper = new TextToSpeechHelper(requireContext());
+
+        if (postcode_notification != null) {
+            boolean hasPermission = (ContextCompat.checkSelfPermission(getContext(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+            if (!hasPermission) {
+                System.out.println("no Permission");
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_STORAGE);
+            } else {
+                System.out.println("Goto tts");
+                textToSpeechHelper.startSynthesizeThread("You have 3 Message in " + postcode_notification);
+                //playAudio();
+            }
+        }else{
+            System.out.println("Didn't get Arguments");
+        }
 
         ImageButton wordButton = binding.button;
         ImageButton nearByMessage = binding.button2;
@@ -87,7 +128,6 @@ public class HomeFragment extends Fragment {
                     Toast.makeText(getActivity(), postcode, Toast.LENGTH_SHORT).show();
                     getNearbyGreetingCards(postcode);
                 }
-
             });
             //getNearbyGreetingCards("BS2 0BU");
         });
@@ -117,6 +157,89 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    /*private void playAudio() {
+        if (audioPath != null) {
+            mediaPlayer = new MediaPlayer();
+            try {
+                mediaPlayer.setDataSource(audioPath);
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+                Toast.makeText(getContext(), "Playing Audio", Toast.LENGTH_SHORT).show();
+
+                mediaPlayer.setOnCompletionListener(mp -> {
+                    mp.release();
+                    mediaPlayer = null;
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(getContext(), "Error playing audio", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getContext(), "Audio file path is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void startSynthesizeThread(final String text) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synthesizeTextToSpeech(text);
+            }
+        }).start();
+    }
+
+    private void synthesizeTextToSpeech(String text) {
+        IamAuthenticator authenticator = new IamAuthenticator("5QNOe6RBy9UBI_4kHQ8iTisGsrIAusAxQfIMydNq8O63");
+        TextToSpeech textToSpeech = new TextToSpeech(authenticator);
+        textToSpeech.setServiceUrl("https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/1766a68d-26f1-439c-a612-370c829aeb42");
+
+        try {
+            SynthesizeOptions synthesizeOptions =
+                    new SynthesizeOptions.Builder()
+                            .text(text)
+                            .accept("audio/mp3")
+                            .voice("en-US_AllisonV3Voice")
+                            .build();
+
+            InputStream inputStream = textToSpeech.synthesize(synthesizeOptions).execute().getResult();
+            InputStream in = WaveUtils.reWriteWaveHeader(inputStream);
+
+            File externalFilesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+            if (externalFilesDir != null) {
+                File outputFile = new File(externalFilesDir, "hello_world_test.mp3");
+                System.out.println("find it");
+
+                try (OutputStream out = new FileOutputStream(outputFile)) {
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = in.read(buffer)) > 0) {
+                        out.write(buffer, 0, length);
+                    }
+
+                    Log.i("MainActivity", "Audio file saved at: " + outputFile.getAbsolutePath());
+                    audioPath = outputFile.getAbsolutePath();
+                    System.out.println("find it" + outputFile.getAbsolutePath());
+
+                    if (isAdded() && getActivity() != null) {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                playAudio();
+                            }
+                        });
+                    }
+                }
+                in.close();
+                inputStream.close();
+            }else{
+                System.out.println("Fail to find dir");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("MainActivity", "Failed to save audio file: " + e.getMessage());
+        }
+    }*/
 
     /**
      * Help user to send card
